528bedbe8a3345fa991ca3e792400095475ce37a
Zachary Clark <zacclark369@gmail.com>
2022-01-30 12:30:53 -0800

    index.js complete. initial questions created. Receiving array added. Will need to expand js files next.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const inquire = require('inquirer');
-const db = require('db');
+const inquirer = require('inquirer');
+const db = require('./assets/envSqlConfig');
+const instructionList = require('./assets/js/instructionList');
+const databaseDirections = new instructionList(db);
+const instructionArray = require('./assets/js/instructionArray');
+
+const start = async () => {
+    const uInstruction = await inquirer.prompt(
+        {
+            type: 'list',
+            name: 'instruction',
+            message: 'How would you like to process',
+            choices: ['View Departments', 'View Roles', 'View Employees', 'Add department', 'Add employees', "Add Role", "Update", 'EXIT']
+        }
+    );
+    
+    const instructArry = uInstruction.instruction.split(' ');
+    const parseUInstruction = instructionArray(instructArry);
+    await databaseDirections[parseUInstruction]();
+    start();
+}
+
+start();
